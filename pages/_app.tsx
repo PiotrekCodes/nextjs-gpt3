e4ddd8995a1e44421e7e4c91a303d2cb0bbfdad6
Piotr Krawczyk <sztymperjan@gmail.com>
2023-02-12 15:58:18 +0000

    backend api added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Lato } from "@next/font/google";
+const lato = Lato({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-lato",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${lato.className} bg-slate-900 min-h-screen`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
